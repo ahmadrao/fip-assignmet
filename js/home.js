1bc2f49d19a05a35504f230831f57651b82f5418
Muhammad Ahmad <ahmadraosanawarali@gmail.com>
2020-04-24 22:39:19 +0500

    Project is almost ready

--- js/home.js
+++ js/home.js
@@ -27,3 +27,32 @@ function backToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  slides[slideIndex - 1].style.height = '100vh';
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
